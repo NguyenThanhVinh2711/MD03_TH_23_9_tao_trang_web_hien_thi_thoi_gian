366eb854689e3bde53671171283f69e772737bc1
NguyenThanhVinh2711 <vinh99657@gmail.com>
2022-09-23 10:15:22 +0700

    first commit

---
+++ src/main/java/com/example/thtaotrangwebhienthithoigian/ServerTimeServlet.java
@@ -0,0 +1,33 @@
+package com.example.thtaotrangwebhienthithoigian;
+
+import java.io.*;
+import java.util.Date;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ServerTimeServlet ", value = "/index")
+public class ServerTimeServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
